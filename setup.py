2165dde6561e17511ac45b5602c072304fdbfb6e
Doronin Stanislav <mugisbrows@gmail.com>
2022-03-06 09:27:58 +0300

    move examples

--- setup.py
+++ setup.py
@@ -6,15 +6,17 @@ with open('readme.md', encoding='utf-8') as f:
 setup(
     packages = find_packages(),
     name = 'pbat',
-    version='0.0.3',
-    author="Stanislav Doronin",
-    author_email="mugisbrows@gmail.com",
-    url='https://github.com/mugiseyebrows/pbat',
-    description='Batch file preprocessor',
+    author = "Stanislav Doronin",
+    author_email = "mugisbrows@gmail.com",
+    url = 'https://github.com/mugiseyebrows/pbat',
+    description = 'Batch file preprocessor',
     long_description = long_description,
     long_description_content_type = 'text/markdown',
     install_requires = [],
-    entry_points={
+    package_data = {
+        'pbat': ['examples/*.pbat', 'examples/*.bat']
+    },
+    entry_points = {
         'console_scripts': [
             'pbat = pbat.compile:main',
             'pbat.compile = pbat.compile:main',
