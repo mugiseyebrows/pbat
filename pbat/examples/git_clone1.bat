a00d51611c97d741e123381eff07d16085b6af15
Doronin Stanislav <mugisbrows@gmail.com>
2023-01-07 00:28:39 +0300

    examples

--- pbat/examples/git_clone1.bat
+++ pbat/examples/git_clone1.bat
@@ -1,11 +1,10 @@
 @echo off
-rem This file is generated from hello5.pbat, all edits will be lost
+rem This file is generated from git_clone1.pbat, all edits will be lost
 if exist "C:\Program Files\Git\cmd\git.exe" set GIT=C:\Program Files\Git\cmd\git.exe
 if not defined GIT (
 echo GIT not found
 exit /b
 )
-set PATH=C:\Program Files\Git\cmd;C:\windows;C:\windows\system32
 if not exist pbat (
 "%GIT%" clone https://github.com/mugiseyebrows/pbat.git
 pushd pbat
