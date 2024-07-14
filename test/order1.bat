5058c83e57b756feca6370a18bf365c858e6ca00
Doronin Stanislav <mugisbrows@gmail.com>
2024-07-14 20:31:08 +0300

    cleanup

--- test/order1.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-rem This file is generated from order1.pbat, all edits will be lost
-rem get_souce
-rem get_compiler
-rem compile
--- test/order2.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-rem This file is generated from order2.pbat, all edits will be lost
-rem foo_dep
-rem foo
-rem bar_dep
-rem bar
